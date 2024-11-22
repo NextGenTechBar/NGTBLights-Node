a3b3995c7e84385659c699902dc0e83c5f8b696e
Leif Forrest <thekinghobbit@gmail.com>
2024-11-22 10:49:42 -0800

    added pride labels and fixed close button

--- ngtb-frontend/src/components/pride.js
+++ ngtb-frontend/src/components/pride.js
@@ -13,7 +13,8 @@ const Pride = (props) => {
         'FRACS001001001100100100255255255104006080001001001100100100255255255104006080001001001100100100255255255104006080',
         'FRACS255216000255255255140089180001001001255216000255255255140089180001001001255216000255255255140089180001001001',
         'FRACS058166064168212122255255255130130130001001001058166064168212122255255255130130130001001001058166064168212122255255255130130130001001001',
-        'FRACS141076200255255255034109025141076200255255255034109025141076200255255255034109025'
+        'FRACS141076200255255255034109025141076200255255255034109025141076200255255255034109025',
+        'FRACS228003003255140000255237000000128038000076255255041255'
     ];
     const images = [
         'url(./trans.png)',
@@ -24,7 +25,20 @@ const Pride = (props) => {
         'url(./ace.png)',
         'url(./nonbinary.png)',
         'url(./aro.png)',
+        'url(./queer.jpg)',
         'url(./Pride.png)'
+    ];
+    const labels = [
+        'Trans', 
+        'Pan',
+        'Bi',
+        'Gay',
+        'Lesbian',
+        'Ace',
+        'Nonbinary',
+        'Aro',
+        'Queer',
+        'Pride'
     ];
     const handleClick = (pride, index) => {
         console.log(`Button ${index + 1} with color ${pride} clicked`);
@@ -37,19 +51,24 @@ const Pride = (props) => {
     };
 
     const buttons = pride.map((color, index) => (
-        <button
-            key={index} 
-            className="pride-button" 
-            style={{ backgroundImage: images[index] }}
+        <div key={index} className="pattern-button-container">
 
-            onClick={() => handleClick(color, index)}
-        >
-            
-        </button>
+            <button
+                key={index} 
+                className="pride-button" 
+                style={{ backgroundImage: images[index] }}
+
+                onClick={() => handleClick(color, index)}
+            >
+                
+            </button>
+            <div className="animation-label">{labels[index]}</div>
+
+        </div>
     ));
     return (
         <div className="static-colors-container">
-            <h1>Patterns!</h1>
+            <h1>Pride Flags!</h1>
             <div className="grid-container">
                 {buttons}
             </div>
